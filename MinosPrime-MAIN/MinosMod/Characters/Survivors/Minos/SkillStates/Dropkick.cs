304385792b9d3c35813455fafdce1327007c5137
kfc-emanes <mr2binary@gmail.com>
2026-04-12 12:12:23 +0800

    Update Dropkick.cs
    + Refined the implementation a bit
    (!) Still needs more polishing but I just wanted to push this out
    - Mostly removed previous template code

--- MinosPrime-MAIN/MinosMod/Characters/Survivors/Minos/SkillStates/Dropkick.cs
+++ MinosPrime-MAIN/MinosMod/Characters/Survivors/Minos/SkillStates/Dropkick.cs
@@ -8,20 +8,19 @@ namespace MinosMod.Survivors.Minos.SkillStates
 {
     public class Dropkick : BaseSkillState
     {
-        public static float duration = 0.7f;
-        public static float delayPercent = 0.4f; //animation threshold to start the dash
-        public static float blitzSpeedCoefficient = 12f; //speed of dash
+        public static float duration = 2f;
+        public static float delayPercent = 0.6f; //animation threshold to start the dash
+        public static float brakePercent = 0.8f; //stopping midair
+        public static float blinkSpeed = 240f; //speed of blink
 
-        private bool hasBlitzed;
-        private Vector3 blitzDirection;
+        private bool hasBlinked;
+        private bool hasBraked;
+        private Vector3 blinkDirection;
 
         public static string dropkickSoundString = "mp_dropkick";
         public static float dodgeFOV = global::EntityStates.Commando.DodgeState.dodgeFOV;
 
         //soon to be deprecated variables, part of template logic
-        private float rollSpeed;
-        private Vector3 forwardDirection;
-        private Vector3 previousPosition;
         private Animator animator;
 
         public override void OnEnter()
@@ -29,36 +28,14 @@ namespace MinosMod.Survivors.Minos.SkillStates
             base.OnEnter();
             animator = GetModelAnimator();
 
-            //if (isAuthority && inputBank && characterDirection)
-            //{
-            //    forwardDirection = (inputBank.moveVector == Vector3.zero ? characterDirection.forward : inputBank.moveVector).normalized;
-            //}
-
-            //Vector3 rhs = characterDirection ? characterDirection.forward : forwardDirection;
-            //Vector3 rhs2 = Vector3.Cross(Vector3.up, rhs);
-
-            //float num = Vector3.Dot(forwardDirection, rhs);
-            //float num2 = Vector3.Dot(forwardDirection, rhs2);
-
-            //RecalculateRollSpeed();
-
-            //if (characterMotor && characterDirection)
-            //{
-            //    characterMotor.velocity.y = 0f;
-            //    characterMotor.velocity = forwardDirection * rollSpeed;
-            //}
-
-            //Vector3 b = characterMotor ? characterMotor.velocity : Vector3.zero;
-            //previousPosition = transform.position - b;
-
             PlayAnimation("Combat, Override", "Dropkick", "Dropkick.playbackRate", duration);
             Util.PlaySound(dropkickSoundString, gameObject);
 
             if (isAuthority && inputBank)
             {
-                blitzDirection = inputBank.aimDirection;
-                blitzDirection.y = 0;
-                blitzDirection.Normalize();
+                blinkDirection = inputBank.aimDirection;
+                blinkDirection.y = 0;
+                blinkDirection.Normalize();
             }
 
             if (NetworkServer.active)
@@ -67,33 +44,30 @@ namespace MinosMod.Survivors.Minos.SkillStates
             }
         }
 
-        //private void RecalculateRollSpeed()
-        //{
-        //    rollSpeed = moveSpeedStat * Mathf.Lerp(initialSpeedCoefficient, finalSpeedCoefficient, fixedAge / duration);
-        //}
-
         public override void FixedUpdate()
         {
             base.FixedUpdate();
 
-            //crouching. freezes minos in place.
             if (fixedAge < duration * delayPercent)
             {
                 if (characterMotor) characterMotor.velocity = Vector3.zero;
             }
-            else
+            else if (fixedAge < duration * brakePercent)
             {
-                if (!hasBlitzed)
+                if (!hasBlinked)
                 {
-                    hasBlitzed = true;
-                    Util.PlaySound("mp_dropkick_launch", gameObject);
-                    if (isAuthority && characterDirection) characterDirection.forward = blitzDirection;
+                    hasBlinked = true;
+                    if (isAuthority && inputBank) blinkDirection = inputBank.aimDirection.normalized;
                 }
 
-                //blitz towards mouse/aim direction
-                if (characterMotor && isAuthority)
+                if (isAuthority && characterMotor) characterMotor.velocity = blinkDirection * blinkSpeed;
+            }
+            else
+            {
+                if (!hasBraked)
                 {
-                    characterMotor.velocity = blitzDirection * (moveSpeedStat * blitzSpeedCoefficient);
+                    hasBraked = true;
+                    if (isAuthority && characterMotor) characterMotor.velocity = Vector3.zero;
                 }
             }
 
@@ -114,13 +88,11 @@ namespace MinosMod.Survivors.Minos.SkillStates
         public override void OnSerialize(NetworkWriter writer)
         {
             base.OnSerialize(writer);
-            writer.Write(forwardDirection);
         }
 
         public override void OnDeserialize(NetworkReader reader)
         {
             base.OnDeserialize(reader);
-            forwardDirection = reader.ReadVector3();
         }
     }
 }
